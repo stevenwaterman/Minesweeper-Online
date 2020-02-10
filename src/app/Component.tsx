5ed5f11b491239823a8d2a43e8faaf3d4aa3a1dd
Steven Waterman <github@stevenwaterman.uk>
2020-02-10 12:24:11 +0000

    add instructions

--- src/app/Component.tsx
+++ src/app/Component.tsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Provider } from "react-redux";
+import Modal from "react-modal";
 
 import "./Styles.scss";
-import Overlay, { targetColor, firstColor, secondColor } from "../hover/ConstraintOverlay";
+import Overlay, {
+  targetColor,
+  firstColor,
+  secondColor
+} from "../hover/ConstraintOverlay";
 import Board from "../board/Board";
 import CoordsOverlay from "../coordsOverlay/CoordsOverlay";
 import AllOptions from "../options/AllOptionsPanel";
@@ -18,8 +23,31 @@ import {
 } from "../constraints/Selectors";
 
 const App: React.FC = () => {
+  const [modalIsOpen, setModalVisible] = useState(true);
+
   return (
     <Provider store={store}>
+      <Modal
+        style={{
+          content: {
+            width: "300px",
+            height: "200px"
+          }
+        }}
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalVisible(false)}
+        contentLabel="Introduction Modal"
+      >
+        <h2>Instructions</h2>
+        <p>
+          If this is your first time using the tool, make sure you read the
+          "instructions" section of the Github Readme. It explains how
+          everything works!
+        </p>
+        <a href="https://github.com/stevenwaterman/Minesweeper-Constrained/blob/master/README.md#instructions">
+          Click me to go there!
+        </a>
+      </Modal>
       <div className="columns">
         <div className="leftColumn">
           <Title />
